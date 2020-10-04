b906b56c313db58cf174d6de36a7593ff71a3e29
nikhil-js-12 <nikhilprakash.js@gmail.com>
2020-10-04 10:07:04 +0530

    Swelp Community Requirements

---
+++ app/src/main/java/com/nikhilstudios12/bed/Hlist.java
@@ -0,0 +1,14 @@
+package com.nikhilstudios12.bed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hlist);
+    }
+}
---
+++ app/src/main/java/com/nikhilstudios12/bed/Service.java
@@ -0,0 +1,14 @@
+package com.nikhilstudios12.bed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+    }
+}
